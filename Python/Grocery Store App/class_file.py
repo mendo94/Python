4d4dbd083feb37278e3c41c4c82024670e627aa2
mendo94 <rinamendozaa@gmail.com>
2022-07-02 17:24:20 -0400

    Transfer python learning to one file

---
+++ Python/Grocery Store App/class_file.py
@@ -0,0 +1,14 @@
+class StoreInfo:
+
+    def __init__(self, location, address):
+        self.location = location
+        self.address = address
+        self.items = []
+
+
+class ShoppingList:
+
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
