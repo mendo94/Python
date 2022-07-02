b9069262c86a49ec860fc6b1ea92c7d84a007425
mendo94 <rinamendozaa@gmail.com>
2022-07-02 18:55:51 -0400

    Merged blackjack

---
+++ Blackjack/Blackjack-using-Python/README.md
@@ -0,0 +1,19 @@
+# Blackjack-using-Python
+
+Blackjack starts by asking the user if they would like to place a bet before beginning the game play. The players are given 100 chips to start. The user makes their bet and the game begins. The cards have each been given a value. For example, the 2 of Clubs will be equal to 2 and royals shall be equal to 10 points. The Ace can either be 1 or 11 points. Get as close to or equal to 21 to win the round. Going over 21 points in a single round will result in a bust! If your hand is higher than the dealer's when you stand, you win, otherwise you lose your bet.
+
+Game Play
+To play a hand of Blackjack the following steps must be followed:
+
+Create a deck of 52 cards
+Shuffle the deck
+Ask the Player for their bet
+Make sure that the Player's bet does not exceed their available chips
+Deal two cards to the Dealer and two cards to the Player
+Show only one of the Dealer's cards, the other remains hidden
+Show both of the Player's cards
+Ask the Player if they wish to Hit, and take another card
+If the Player's hand doesn't Bust (go over 21), ask if they'd like to Hit again.
+If a Player Stands, play the Dealer's hand. The dealer will always Hit until the Dealer's value meets or exceeds 17
+Determine the winner and adjust the Player's chips accordingly
+Ask the Player if they'd like to play again
---
+++ Blackjack/Blackjack-using-Python/__pycache__/blackjack/blackjack_classes.py
@@ -0,0 +1,67 @@
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
+         'Queen':10, 'King':10, 'Ace':11}
+
+class Card:
+
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+        
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
+
+class Deck:
+    
+    def __init__(self):
+        self.deck = []  # start with an empty list
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))  # build Card objects and add them to the list
+    
+    def __str__(self):
+        deck_comp = ''  # start with an empty string
+        for card in self.deck:
+            deck_comp += '\n '+card.__str__() # add each Card object's print string
+        return 'The deck has:' + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+        
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
+class Hand:
+    
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0   # start with zero value
+        self.aces = 0    # add an attribute to keep track of aces
+    
+    def add_card(self,card):
+      # card passed in
+      # from Deck.deal() --> single Card(suit, rank)
+        self.cards.append(card)
+        self.value += values[card.rank]
+        if card.rank == 'Ace':
+            self.aces += 1  # add to self.aces
+    
+    def adjust_for_ace(self):
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1 
+          
+class Chips:
+    
+    def __init__(self):
+        self.total = 100  # This can be set to a default value or supplied by a user input
+        self.bet = 0
+        
+    def win_bet(self):
+        self.total += self.bet
+    
+    def lose_bet(self):
+        self.total -= self.bet
